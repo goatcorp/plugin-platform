599bac050cf3e7d602751cb2c3a05b4ee8c2638e
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-08-19 21:18:00 -0700

    Add all presets page

---
+++ client/src/routes/presets/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import PresetSearchResult from '$lib/PresetRow.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<h1>All presets</h1>
+
+<div>
+	{#each data.presets as preset}
+		<PresetSearchResult
+			{preset}
+			authorName={data.authors[preset.author]}
+			stats={data.stats[preset.id]}
+		/>
+	{/each}
+</div>
+
+<style lang="scss">
+	* {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+			'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+</style>
---
+++ client/src/routes/presets/+page.ts
@@ -0,0 +1,47 @@
+import PocketBase from 'pocketbase';
+import type { Preset, PresetStats } from '$lib/preset';
+
+const connect = () => {
+	return new PocketBase('http://127.0.0.1:8090');
+};
+
+export async function load({ url }: { url: URL }) {
+	const client = connect();
+	const records = await client.records.getList('presets', 1);
+	const presets: Preset[] = records.items.map((item) => ({
+		id: item.id,
+		thumbnail: item.thumbnail,
+		title: item.title,
+		author: item.author,
+		created: new Date(item.created),
+		updated: new Date(item.updated)
+	}));
+
+	const stats: Record<string, PresetStats> = {};
+	for (const preset of presets) {
+		const presetStatsRecords = await client.records.getFullList('preset_stats', undefined, {
+			filter: `preset~'${preset.id}'`
+		});
+		stats[preset.id] = presetStatsRecords
+			.map((record) => ({
+				views: record.views
+			}))
+			.reduce(
+				(agg, next) => {
+					agg.views += next.views;
+					return agg;
+				},
+				{
+					views: 0
+				}
+			);
+	}
+
+	const authors: Record<string, string> = {};
+	for (const result of presets) {
+		const user = await client.records.getOne('profiles', result.author);
+		authors[result.author] = user.name;
+	}
+
+	return { presets, authors, stats };
+}
