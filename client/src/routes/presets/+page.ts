939b9502c1d8f60f49122f064cb7cb4e10a14234
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-08-20 13:43:09 -0700

    Remove unused import

--- client/src/routes/presets/+page.ts
+++ client/src/routes/presets/+page.ts
@@ -1,7 +1,6 @@
 import PocketBase from 'pocketbase';
 import type { Preset, PresetStats } from '$lib/preset';
 import type { PageLoad } from './$types';
-import { getSettings, type Settings } from '$lib/settings';
 
 const connect = () => {
 	return new PocketBase('http://127.0.0.1:8090');
