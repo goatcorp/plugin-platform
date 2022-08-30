9a54e25b1114ae998976ee7313acf5491501afd7
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-08-30 14:06:30 -0700

    Add default layout

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -21,60 +21,86 @@
 	});
 </script>
 
-<header>
-	<nav>
-		<div><a href="/">Dalamud Plugin Presets</a></div>
-		<div class="controls">
-			<div class="search">
-				<div>
-					<form method="get" action="/search">
-						<input type="search" placeholder="Search for presets..." name="q" />
-						<button>Search</button>
-					</form>
+<div class="layout">
+	<header class="header">
+		<nav>
+			<div><a href="/">Dalamud Plugin Presets</a></div>
+			<div class="controls">
+				<div class="search">
+					<div>
+						<form method="get" action="/search">
+							<input type="search" placeholder="Search for presets..." name="q" />
+							<button>Search</button>
+						</form>
+					</div>
 				</div>
+				{#if user == null}
+					<a href="/login">Log in</a>
+				{:else}
+					<a href="/logout">Log out</a>
+					<a href="/create">Create</a>
+					<a href="/settings">Settings</a>
+					<a href={`/user/${user.profile?.id}`}>{user.profile?.name}</a>
+				{/if}
 			</div>
-			{#if user == null}
-				<a href="/login">Log in</a>
-			{:else}
-				<a href="/logout">Log out</a>
-				<a href="/create">Create</a>
-				<a href="/settings">Settings</a>
-				<a href={`/user/${user.profile?.id}`}>{user.profile?.name}</a>
-			{/if}
-		</div>
-	</nav>
-</header>
+		</nav>
+	</header>
 
-<main><slot /></main>
+	<main class="content"><slot /></main>
+
+	<nav class="sidebar">
+		<h1>Right text</h1>
+	</nav>
+
+	<footer class="footer">
+		<p>Bottom text</p>
+	</footer>
+</div>
 
 <style lang="scss">
-	header {
-		margin: auto;
-		padding: 20px;
-		width: 70%;
+	.layout {
+		display: grid;
+		grid-template: auto / calc(100% - 400px) 400px;
 
-		nav {
-			display: flex;
-			justify-content: space-between;
+		.header {
+			grid-column: 1 / 3;
+			padding: 35px;
 
-			.controls {
+			nav {
 				display: flex;
-				justify-content: right;
+				justify-content: space-between;
 
-				> * {
-					margin-left: 8px;
+				.controls {
+					display: flex;
+					justify-content: right;
+
+					> * {
+						margin-left: 8px;
+					}
+				}
+
+				.search {
+					display: flex;
 				}
 			}
-
-			.search {
-				display: flex;
-			}
 		}
-	}
 
-	main {
-		width: 70%;
-		margin: auto;
+		.content {
+			grid-column: 1;
+			padding-right: 35px;
+			padding-left: 35px;
+		}
+
+		.sidebar {
+			grid-column: 2;
+			padding-right: 35px;
+			padding-left: 35px;
+		}
+
+		.footer {
+			grid-column: 1 / 3;
+			padding: 35px;
+		}
 	}
 
 	* {
