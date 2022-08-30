8e9991609c865005d207459e20a72dd305396fe3
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-08-30 14:42:15 -0700

    Close menu upon clicking a menu item

--- client/src/lib/components/UserMenu.svelte
+++ client/src/lib/components/UserMenu.svelte
@@ -5,23 +5,27 @@
 	export let username: string;
 
 	let open = false;
+
+	const toggleMenu = () => {
+		open = !open;
+	};
+
+	const closeMenu = () => {
+		open = false;
+	};
 </script>
 
 <div>
-	<button
-		on:click={() => {
-			open = !open;
-		}}
-	>
+	<button on:click={toggleMenu}>
 		<UserIcon />
 	</button>
 
 	<div class="mount-point">
 		{#if open}
 			<div class="user-menu">
-				<a href={`/user/${id}`}>{username}</a>
-				<a href="/settings">Settings</a>
-				<a href="/logout">Log out</a>
+				<a href={`/user/${id}`} on:click={closeMenu}>{username}</a>
+				<a href="/settings" on:click={closeMenu}>Settings</a>
+				<a href="/logout" on:click={closeMenu}>Log out</a>
 			</div>
 		{/if}
 	</div>
